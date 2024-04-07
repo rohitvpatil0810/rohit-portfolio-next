c488cd8ab6b398f5b376f4dbe95d998334dce44f
Rohit Patil <rohitvpatil0810@gmail.com>
2024-04-08 00:28:08 +0530

    feat: add hero section

---
+++ src/components/hero.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+function Hero({}: Props) {
+  return (
+    <div className="flex justify-between items-center h-screen">
+      <div className="w-1/2">
+        <h2 className="text-5xl font-semibold py-1">Hi, I'm</h2>
+        <h1 className="text-8xl font-bold py-1 text-light-green">
+          Rohit Patil
+        </h1>
+        <h3 className="text-3xl font-semibold text-gray-200 py-1">
+          A Passionate Software Engineer and Computer Engineering Student from
+          India 🇮🇳
+        </h3>
+      </div>
+      <Image
+        src="/developer1.svg"
+        alt="Picture of the author"
+        width={700}
+        height={700}
+      />
+    </div>
+  );
+}
+
+export default Hero;
