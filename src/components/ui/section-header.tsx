76f69dc17fd199d9b51dc0822b194506db663954
Rohit Patil <rohitvpatil0810@gmail.com>
2024-04-08 00:46:31 +0530

    feat: add section-header ui element

---
+++ src/components/ui/section-header.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+type Props = {
+  className?: string;
+  children: ReactNode;
+};
+
+function SectionHeader({ className = "", children }: Props) {
+  return (
+    <div
+      className={`text-sm px-4 py-2 border-2 border-light-green rounded-md w-fit font-semibold text-light-green ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default SectionHeader;
