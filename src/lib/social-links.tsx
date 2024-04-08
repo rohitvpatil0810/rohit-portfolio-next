e43233f1f89637b834de483a340e039d2dc7084e
Rohit Patil <rohitvpatil0810@gmail.com>
2024-04-09 00:44:22 +0530

    chore: add social links

---
+++ src/lib/social-links.tsx
@@ -0,0 +1,25 @@
+import {
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandX,
+} from "@tabler/icons-react";
+
+export const socialLinks = [
+  {
+    icon: <IconBrandGithub />,
+    sociallink: "https://github.com/rohitvpatil0810/",
+  },
+  {
+    icon: <IconBrandLinkedin />,
+    sociallink: "https://www.linkedin.com/in/rohitvpatil0810/",
+  },
+  {
+    icon: <IconBrandInstagram />,
+    sociallink: "https://www.instagram.com/rohitvpatil0810/",
+  },
+  {
+    icon: <IconBrandX />,
+    sociallink: "https://www.twitter.com/rohitvpatil0810/",
+  },
+];
