390a80358cf8fd2d3eed301d0e83853785525c15
Rohit Patil <rohitvpatil0810@gmail.com>
2024-04-09 23:06:42 +0530

    feat: add className attribute to Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,12 +1,13 @@
 interface ButtonProps {
   onClick?: () => void;
   text: string;
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, text, className }) => {
   return (
     <button
-      className="bg-light-green px-4 py-2 rounded-lg text-eerie-black font-semibold border-2 border-light-green hover:border-green-500"
+      className={`bg-light-green px-4 py-2 rounded-lg text-eerie-black font-semibold border-2 border-light-green hover:border-green-500 ${className}`}
       onClick={onClick}
     >
       {text}
