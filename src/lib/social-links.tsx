ba86f96b30ac53ec4a8fe3e31d94a319bafbd5d6
Rohit Patil <rohitvpatil0810@gmail.com>
2024-06-28 00:07:57 +0530

    chore: update social icon component

--- src/lib/social-links.tsx
+++ src/lib/social-links.tsx
@@ -9,17 +9,21 @@ export const socialLinks = [
   {
     icon: <IconBrandGithub />,
     sociallink: "https://github.com/rohitvpatil0810/",
+    username: "rohitvpatil0810",
   },
   {
     icon: <IconBrandLinkedin />,
     sociallink: "https://www.linkedin.com/in/rohitvpatil0810/",
+    username: "in/rohitvpatil0810",
   },
   {
     icon: <IconBrandInstagram />,
     sociallink: "https://www.instagram.com/rohitvpatil0810/",
+    username: "rohitvpatil0810",
   },
   {
     icon: <IconBrandX />,
     sociallink: "https://www.twitter.com/rohitvpatil0810/",
+    username: "rohitvpatil0810",
   },
 ];
