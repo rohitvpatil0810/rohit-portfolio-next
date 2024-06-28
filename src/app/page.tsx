5907af39302fcb7c66c38a5f594167555ddf4384
Rohit Patil <rohitvpatil0810@gmail.com>
2024-06-28 22:29:38 +0530

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from "@/components/sections/about-me";
 import Contact from "@/components/sections/contact";
 import Education from "@/components/sections/education";
 import Experience from "@/components/sections/experience";
+import Footer from "@/components/sections/footer";
 import Hero from "@/components/sections/hero";
 import Navbar from "@/components/sections/navbar";
 import Projects from "@/components/sections/projects";
@@ -18,6 +19,7 @@ export default function Home() {
       <Experience />
       <Education />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/sections/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <div>
+      <footer className="text-white text-center py-4">
+        <p>🎯 Never Stop Learning - Rohit Patil</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
