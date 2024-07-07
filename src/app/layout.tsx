41bc278973de711f73695c6c1b2464786521dfee
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-08 00:00:53 +0530

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: "Rohit Patil | Software Engineer",
   description:
     "Hello, I&apos;m Rohit Patil, a computer engineering student currently studying at Pune Institute of Computer Technology. As @rohitvpatil0810, my passion lies in software development, and my portfolio reflects a blend of creativity and technical expertise. Projects like &apos;Notes pRO&apos; and &apos;News Ninja&apos; showcase my dynamic skill set, while the MERN stack-powered Public Health Record Management System reflects my commitment to secure and seamless experiences. As I pursue my degree, I am eager to explore opportunities in software engineering. Feel free to delve into my portfolio and join me in this exciting journey of code, design, and innovation! 🚀 #ComputerEngineeringStudent",
+  verification: {
+    google: process.env.GOOGLE_VERIFICATION_CODE,
+  },
 };
 
 export const viewport: Viewport = {
