7c58b3fd1bdfaf5138f8a60c7614ff02176471c4
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-07 23:29:57 +0530

    feat: add responsiveness to contact section

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -14,9 +14,11 @@ function Contact({}: Props) {
       className="w-full py-10 flex flex-col justify-center items-center scroll-mt-10"
     >
       <SectionHeader>Contact</SectionHeader>
-      <h1 className="py-8 text-4xl text-gray-200 font-bold">Get in Touch 📬</h1>
-      <div className="flex justify-center items-end space-x-32">
-        <div className="flex flex-col justify-start space-y-2">
+      <h1 className="py-4 md:py-8 text-2xl sm:text-3xl md:text-4xl text-gray-200 font-bold text-center">
+        Get in Touch 📬
+      </h1>
+      <div className="flex flex-col-reverse md:flex-row justify-center items-center md:items-end md:space-x-32">
+        <div className="pt-10 md:pt-0 flex flex-wrap md:flex-col justify-start md:space-y-2">
           {socialLinks.map((social) => (
             <SocialIcon
               key={social.username + social.sociallink}
