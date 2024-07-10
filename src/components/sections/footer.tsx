329da64759fe9e19fd9ce5a64f4bc880788202b6
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-10 16:44:32 +0530

    refactor: update footer and update about-me

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,3 +1,5 @@
+import aboutMe from "@/lib/about-me";
+import { IconBrandGithub } from "@tabler/icons-react";
 import React from "react";
 
 type Props = {};
@@ -7,6 +9,18 @@ const Footer = (props: Props) => {
     <div>
       <footer className="text-white text-center py-4">
         <p>🎯 Never Stop Learning - Rohit Patil</p>
+        <p className="flex flex-col sm:flex-row justify-center items-center mt-2 text-gray-200 hover:text-white hover:cursor-pointer">
+          <span className="mr-2">View source code at</span>
+          <a
+            href={aboutMe.portfolioRepo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex justify-center items-center hover:text-light-green"
+          >
+            <IconBrandGithub className="mr-2" />
+            <span>rohitvpatil0810/rohit-portfolio-next</span>
+          </a>
+        </p>
       </footer>
     </div>
   );
--- src/lib/about-me.tsx
+++ src/lib/about-me.tsx
@@ -6,6 +6,7 @@ const aboutMe = {
     "Hey there! 👋 I'm Rohit Patil, a computer engineering student at Pune Institute of Computer Technology. 💻 I'm all about building the engines behind apps and websites. I love working on the backend, making sure everything runs smoothly and efficiently. Whether it's setting up databases, creating APIs, or solving tricky problems, I'm here to make sure things work like a charm. Let's team up and bring our ideas to life! 🚀",
   resumeLink:
     "https://drive.google.com/file/d/1RIylVko-_jGaG6_YU6sYHh9xO6qY8BW6/view",
+  portfolioRepo: "https://github.com/rohitvpatil0810/rohit-portfolio-next/",
 };
 
 export default aboutMe;
