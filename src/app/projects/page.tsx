df5819f9469d147f635f56c44b8e33c915215d32
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-10 15:25:13 +0530

    feat: update metadata on projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,9 @@
 import Projects from "@/components/sections/projects/projects";
+import { projectPageMetaData } from "@/lib/meta-data";
 
 type Props = {};
+
+export const metadata = projectPageMetaData;
 
 function ProjectsPage({}: Props) {
   return (
--- src/lib/meta-data.tsx
+++ src/lib/meta-data.tsx
@@ -6,6 +6,7 @@ const pageDetails = {
     "Rohit Patil is a software engineer specializing in full-stack development, creating robust, scalable solutions across software products.",
   name: "Rohit Patil",
   twitterUsername: "@rohitvpatil0810",
+  imagePath: "/og.png",
 };
 
 export const metaData: Metadata = {
@@ -29,7 +30,7 @@ export const metaData: Metadata = {
     siteName: pageDetails.name,
     images: [
       {
-        url: "/og.png",
+        url: pageDetails.imagePath,
         width: 1200,
         height: 630,
         alt: pageDetails.title,
@@ -45,8 +46,66 @@ export const metaData: Metadata = {
     description: pageDetails.description,
     images: [
       {
-        url: "/og.png",
+        url: pageDetails.imagePath,
         alt: pageDetails.title,
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
+const projectPageDetails = {
+  ...pageDetails,
+  title: "Rohit Patil | Projects",
+  imagePath: "/og-projects.png",
+};
+
+export const projectPageMetaData: Metadata = {
+  title: projectPageDetails.title,
+  description: projectPageDetails.description,
+  authors: [{ name: projectPageDetails.name }],
+  creator: projectPageDetails.name,
+  keywords: [
+    projectPageDetails.name,
+    "Software Engineer",
+    "Full-Stack Developer",
+    "Projects",
+    "Rohit Patil Projects",
+  ],
+  publisher: projectPageDetails.name,
+  verification: {
+    google: process.env.GOOGLE_VERIFICATION_CODE,
+  },
+  metadataBase: new URL(
+    `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/`
+  ),
+  openGraph: {
+    type: "website",
+    title: projectPageDetails.title,
+    description: projectPageDetails.description,
+    locale: "en_US",
+    siteName: projectPageDetails.name,
+    images: [
+      {
+        url: projectPageDetails.imagePath,
+        width: 1200,
+        height: 630,
+        alt: projectPageDetails.title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: projectPageDetails.name,
+    creator: projectPageDetails.twitterUsername,
+    creatorId: projectPageDetails.twitterUsername,
+    title: projectPageDetails.title,
+    description: projectPageDetails.description,
+    images: [
+      {
+        url: projectPageDetails.imagePath,
+        alt: projectPageDetails.title,
         width: 1200,
         height: 630,
       },
