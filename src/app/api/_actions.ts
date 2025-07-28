ce7cbf2e54b7efb0775a4152fd4371cc3cf13ae5
Rohit Patil <rohitvpatil0810@gmail.com>
2025-07-28 23:11:05 +0530

    chore: update apiURL for sendEmailNotification

--- src/app/api/_actions.ts
+++ src/app/api/_actions.ts
@@ -8,7 +8,7 @@ const sendEmailNotification = async (data: IFormInput) => {
 
   try {
     const response = await axios.post(
-      apiUrl + "/email/send-notification",
+      apiUrl!,
       data,
       {
         headers: {
